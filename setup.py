b93eb3b74bba7c3e39193e222d76e287862bcbe6
Daniel <daniel@courbanize.com>
2014-07-02 17:46:35 +0100

    Version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-raster',
-    version='0.1.5',
+    version='0.1.6',
     packages=['raster'],
     include_package_data=True,
     license='BSD',
