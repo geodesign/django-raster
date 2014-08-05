7725c95a144c5224ab77220644d50efd782acd7f
Daniel <daniel@courbanize.com>
2014-08-05 18:24:05 +0100

    Major improvement, including tests and docs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-raster',
-    version='0.1.8',
+    version='0.2.0',
     packages=['raster', 'raster.migrations'],
     include_package_data=True,
     license='BSD',
