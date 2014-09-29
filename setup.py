9a8dca89cef03233b44027578656bfa3785778fb
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2014-09-29 12:47:08 +0100

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-raster',
-    version='0.1.2',
+    version='0.1.1',
     packages=['raster', 'raster.migrations'],
     include_package_data=True,
     license='BSD',
     description='Simple raster file implementation for Django based on PostGis',
     long_description=README,
     url='https://github.com/geodesign/django-raster',
-    download_url = 'https://github.com/geodesign/django-raster/tarball/v0.1.2',
+    download_url = 'https://github.com/geodesign/django-raster/tarball/0.1.1',
     author='Daniel Wiesmann',
     author_email='daniel@urbmet.com',
     install_requires=[
