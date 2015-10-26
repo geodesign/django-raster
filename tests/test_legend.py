2fc10f6ca3cb1f1d72fe271316ac5c4c40247ce2
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2015-10-26 14:50:43 +0000

    Improved testing of legend signals

--- tests/test_legend.py
+++ tests/test_legend.py
@@ -6,24 +6,40 @@ from raster.models import Legend, LegendEntry, LegendSemantics
 
 class RasterLegendTests(TestCase):
 
-    def test_raster_legend_creation_and_signals(self):
-        sem1 = LegendSemantics.objects.create(name='Earth')
+    def setUp(self):
+        self.sem1 = LegendSemantics.objects.create(name='Earth')
         sem2 = LegendSemantics.objects.create(name='Wind')
 
-        ent1 = LegendEntry.objects.create(semantics=sem1, expression='1', color='#123456')
+        self.ent1 = LegendEntry.objects.create(semantics=self.sem1, expression='1', color='#123456')
         ent2 = LegendEntry.objects.create(semantics=sem2, expression='2', color='#654321')
 
-        leg = Legend.objects.create(title='MyLegend')
-        leg.entries.add(ent1, ent2)
-        self.assertTrue({"color": "#123456", "expression": "1", "name": "Earth"} in json.loads(leg.json))
-        self.assertTrue({"color": "#654321", "expression": "2", "name": "Wind"} in json.loads(leg.json))
+        self.leg = Legend.objects.create(title='MyLegend')
+        self.leg.entries.add(self.ent1, ent2)
 
-        ent1.color = '#000000'
-        ent1.save()
-        leg = Legend.objects.get(id=leg.id)  # Reload from db
+    def test_raster_legend_json_string(self):
+        self.assertEqual(
+            [
+                {"color": "#123456", "expression": "1", "name": "Earth"},
+                {"color": "#654321", "expression": "2", "name": "Wind"}
+            ],
+            json.loads(self.leg.json)
+        )
+
+    def test_raster_legend_change_semantics_signal(self):
+        self.sem1.name = 'Fire'
+        self.sem1.save()
+        leg = Legend.objects.get(id=self.leg.id)  # Reload from db
+        self.assertTrue({"color": "#123456", "expression": "1", "name": "Fire"} in json.loads(leg.json))
+
+    def test_raster_legend_change_legend_entry_signal(self):
+        self.ent1.color = '#000000'
+        self.ent1.save()
+        leg = Legend.objects.get(id=self.leg.id)  # Reload from db
         self.assertTrue({"color": "#000000", "expression": "1", "name": "Earth"} in json.loads(leg.json))
 
-        sem1.name = 'Fire'
-        sem1.save()
-        leg = Legend.objects.get(id=leg.id)  # Reload from db
-        self.assertTrue({"color": "#000000", "expression": "1", "name": "Fire"} in json.loads(leg.json))
+    def test_raster_legend_entry_list_change_signal(self):
+        self.leg.entries.remove(self.ent1)
+        self.assertEqual(
+            [{"color": "#654321", "expression": "2", "name": "Wind"}],
+            json.loads(self.leg.json)
+        )
