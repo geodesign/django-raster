7c96c3a9f175c0fd515739b51fbc1c12ea05e4da
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2015-11-13 08:28:44 +0000

    Added help text for rasterlayer nodata field.

--- raster/models.py
+++ raster/models.py
@@ -35,9 +35,8 @@ class LegendEntry(models.Model):
     """
     semantics = models.ForeignKey(LegendSemantics)
     expression = models.CharField(max_length=500,
-            help_text='Use a number or a valid numpy logical expression '
-                      'where x is the pixel value. For instance: "(-3.0 < x) '
-                      '& (x <= 1)" or "x <= 1".')
+        help_text='Use a number or a valid numpy logical expression where x is the'
+                  'pixel value. For instance: "(-3.0 < x) & (x <= 1)" or "x <= 1".')
     color = RGBColorField()
 
     def __str__(self):
@@ -133,10 +132,11 @@ class RasterLayer(models.Model, ValueCountMixin):
 
     name = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    datatype = models.CharField(max_length=2, choices=DATATYPES,
-                                default='co')
+    datatype = models.CharField(max_length=2, choices=DATATYPES, default='co')
     rasterfile = models.FileField(upload_to='rasters', null=True, blank=True)
-    nodata = models.CharField(max_length=100, null=True, blank=True)
+    nodata = models.CharField(max_length=100, null=True, blank=True,
+        help_text='Leave blank to keep the internal band nodata values. If a nodata'
+                  'value is specified here, it will be used for all bands of this raster.')
     legend = models.ForeignKey(Legend, blank=True, null=True)
     modified = models.DateTimeField(auto_now=True)
 
