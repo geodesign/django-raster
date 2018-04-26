8ab60c894d4b75f081cb4a33e8d12a1deba81b6d
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2018-04-26 11:49:36 +0100

    Added Python 3.6 to test build and supported versions list.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Scientific/Engineering :: GIS',
